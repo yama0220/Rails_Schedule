32b8b207cb6a4a5617fd7e73c286d44bc516b2b8
山形　彰仁 <yama@yamagataakihitonoMacBook-Pro.local>
2023-04-12 17:40:32 +0900

    Rails課題初回提出

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Schedule
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Tokyo" # タイムゾーンの指定
+    config.action_controller.permit_all_parameters = true
+    config.i18n.default_locale = :ja
   end
 end
